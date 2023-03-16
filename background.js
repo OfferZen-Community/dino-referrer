193ed4bb1450a106853b94571cfe42072f4a4ae6
Stephen van der Heijden <stephengrantvdh@gmail.com>
2023-03-17 00:02:06 +0100

    Changes the way the extention works to listen for chat bubbles everywhere, not just in messaging.
    Moves the logic of when to run what into the linkedin.js content script.
    Loads a dino for every messaging form (added when the click happens)

--- background.js
+++ background.js
@@ -14,19 +14,3 @@ chrome.runtime.onInstalled.addListener(({ reason, version }) => {
     const url = chrome.runtime.getURL('options.html');
     chrome.tabs.create({ url });
   }
-
-  
-  chrome.webNavigation.onHistoryStateUpdated.addListener(details => {
-    if (details.url.indexOf('linkedin.com/messaging') !== -1) {
-      // Execute the 'linkedin.js' content script in the current tab
-      const tabId = details.tabId
-      chrome.scripting.executeScript({
-      target : {tabId : tabId},
-      files : [ "linkedin.js" ],
-    })
-    }
-  }, { url: [{ hostEquals: 'www.linkedin.com' }] });
-  
-
-  
-  
--- linkedin.js
+++ linkedin.js
@@ -1,30 +1,34 @@
+function findAncestor(el, selector) {
+  while ((el = el.parentElement) && !el.matches(selector));
+  return el;
+}
+
+// Run pollDOM() if the page loads on the messaging URL
+if (window.location.href.startsWith('https://www.linkedin.com/messaging') || window.location.href.startsWith('https://www.linkedin.com/inmail')) {
+  pollDOM();
+}
+
+// Add click event listener to the document
+document.addEventListener('click', (event) => {
+  // Check if the clicked element or its ancestor matches the desired selector
+  const targetElement = event.target.matches('.msg-conversation-listitem__link') ? event.target : findAncestor(event.target, '.msg-conversation-listitem__link');
+
+  // If the desired element is found, run pollDOM()
+  if (targetElement) {
+    pollDOM();
+  }
+});
 
 
 function pollDOM() {
+  console.log("Running PollDom")
  //Wait for the toolbar to appear on the page
   const toolbar = document.querySelector('.msg-form__left-actions');
-  const dino = document.getElementById('dino_action')
-  if (dino) {
-    return // Abandon Script if the dino is already there
-  }
-  if (toolbar) {
-//Add a button to click on. Totally just copy pasted here - should probably clean this up at some point. 
-var item = document.createElement("div")
-item.innerHTML = `<div id = "dino_action"><span tabindex="0" class="artdeco-hoverable-trigger artdeco-hoverable-trigger--content-placed-top ember-view"><button title="OfferZen Dino Referrer" aria-label="OfferZen Dino Referrer" aria-expanded="false" aria-controls="artdeco-hoverable-msg_pillar_emoji__emoji-hoverable__content" class="msg-form__footer-action emoji-hoverable-trigger artdeco-button artdeco-button--circle artdeco-button--muted artdeco-button--2 artdeco-button--tertiary ember-view" type="button">  <li-icon aria-hidden="true" class="artdeco-button__icon"><img src="https://i.ibb.co/vmmV6CY/dinocorn.png" height="25px"> </li-icon><span class="artdeco-button__text">Send OfferZen Referral/span></button></span> <div id="msg_pillar_emoji__emoji-hoverable__content" class="ember-view"><class="ember-view"></div></div></div>`
-//Add a listener when the user clicks the button
-item.addEventListener("click", function() {
-    
-    //Set the text in the box, and fire an input event
-    updateMessage(function(updatedMessage) {
-        const messageBox = document.querySelector('.msg-form__contenteditable[role="textbox"]');
-        messageBox.innerHTML = updatedMessage;
-        const inputEvent = new Event('input', { bubbles: true });
-        messageBox.dispatchEvent(inputEvent);
-    });
-});
 
-//Add the button
-toolbar.appendChild(item)
+  if (toolbar) {
+
+//Add a listener when the user clicks the button
+runUpdates()
 
 //If ther toolbar isn't there yet, try again later.
   } else {
@@ -32,17 +36,63 @@ toolbar.appendChild(item)
     setTimeout(pollDOM, 300); // try again in 300 milliseconds
   }
 }
+function runUpdates(){
 
-pollDOM();
+  //Get the message from storage
+chrome.storage.sync.get({
+  referralCode: 'Please check your settings in the Dino Referrer Set Up',
+  referralMessage: 'Message Error'
+}, function(items) {
+  var link = "\r\nhttps://www.offerzen.com/hire-developers?utm_source=referral&utm_medium=dinoref&utm_campaign="
+  var cleanedMessage = items.referralMessage.replace(/\n/g, "</p><p>");
+  const updatedMessage = `<p>` + cleanedMessage + "</p><p>" + link + `${items.referralCode}</p>`;
+  console.log(updatedMessage)
+  appendDinoAction(updatedMessage);
+});
 
-function updateMessage(callback){
-  chrome.storage.sync.get({
-    referralCode: 'PLease check your settings in the Dino Referrer Set Up',
-    referralMessage: 'Message Error'
-  }, function(items) {
-    var link = "\r\nhttps://www.offerzen.com/hire-developers?utm_source=referral&utm_medium=dinoref&utm_campaign="
-    var cleanedMessage = items.referralMessage.replace(/\n/g, "</p><p>");
-    const updatedMessage = `<p>` + cleanedMessage + "</p><p>" + link + `${items.referralCode}</p>`;
-    callback(updatedMessage);
+
+function appendDinoAction(updatedMessage) {
+  const leftActions = document.querySelectorAll('.msg-form__left-actions');
+
+  leftActions.forEach((action) => {
+    // Check if the element has already been appended
+    const existingDinoAction = action.querySelector('#dino_action');
+    if (!existingDinoAction) {
+      // Create the new element
+      const dinoAction = document.createElement('div');
+      dinoAction.id = 'dino_action';
+      dinoAction.innerHTML = `
+        <span tabindex="0" class="artdeco-hoverable-trigger artdeco-hoverable-trigger--content-placed-top ember-view">
+          <button title="OfferZen Dino Referrer" aria-label="OfferZen Dino Referrer" aria-expanded="false" aria-controls="artdeco-hoverable-msg_pillar_emoji__emoji-hoverable__content" class="msg-form__footer-action emoji-hoverable-trigger artdeco-button artdeco-button--circle artdeco-button--muted artdeco-button--2 artdeco-button--tertiary ember-view" type="button">
+            <li-icon aria-hidden="true" class="artdeco-button__icon">
+              <img src="https://i.ibb.co/vmmV6CY/dinocorn.png" height="25px">
+            </li-icon>
+            <span class="artdeco-button__text">Send OfferZen Referral</span>
+          </button>
+        </span>
+        <div id="msg_pillar_emoji__emoji-hoverable__content" class="ember-view">
+          <class="ember-view"></div>
+        </div>
+      `;
+
+//Add the listener
+      const dinoActionButton = dinoAction.querySelector('button');
+        dinoActionButton.addEventListener("click", function (event) {
+          console.log("Clicked Dino")
+          const parentForm = event.target.closest("form");
+          const messageBox = parentForm.querySelector('.msg-form__contenteditable[role="textbox"]');
+          messageBox.innerHTML = updatedMessage;
+          const inputEvent = new Event('input', { bubbles: true });
+          messageBox.dispatchEvent(inputEvent);
+        });
+        
+      // Append the new element
+      action.appendChild(dinoAction);
+    }
   });
 }
+}
+
+
+
+
