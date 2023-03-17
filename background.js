3a87143b6cac6d587e090075e1681ad764609ab1
Stephen van der Heijden <stephengrantvdh@gmail.com>
2023-03-17 11:42:25 +0100

    Ensure it also runs when navigating to messages section

--- background.js
+++ background.js
@@ -14,3 +14,15 @@ chrome.runtime.onInstalled.addListener(({ reason, version }) => {
     const url = chrome.runtime.getURL('options.html');
     chrome.tabs.create({ url });
   }
+    
+  chrome.webNavigation.onHistoryStateUpdated.addListener(details => {
+    if (details.url.indexOf('linkedin.com/messaging') !== -1) {
+      // Execute the 'linkedin.js' content script in the current tab if the user navigated to the messaging page
+      const tabId = details.tabId
+      chrome.scripting.executeScript({
+      target : {tabId : tabId},
+      files : [ "linkedin.js" ],
+    })
+    }
+  }, { url: [{ hostEquals: 'www.linkedin.com' }] });
+  
